9a30b238e37438575d38ef510924a71edeb2a755
MENAKAANBUKKARASU <menakaanbukkarasu@gmail.com>
2024-04-19 14:58:06 +0000

    updated files in developer

---
+++ script.js
@@ -0,0 +1,11 @@
+function greetGitHub() {
+    alert("Hello, GitHub!");
+}
+
+// Get the button element by its ID
+var greetButton = document.getElementById("greetButton");
+
+// Add a click event listener to the button
+greetButton.addEventListener("click", greetGitHub);
+
+
